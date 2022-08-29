aabc57d225af68a9fcc634593523477a78ca1fa0
那須ゆえ <NasuYue@users.noreply.github.com>
2022-08-30 01:04:56 +0800

    Update day3_Drawing-Marbles.py

--- HackerRank/10-Days-of-Statistics/day3_Drawing-Marbles.py
+++ HackerRank/10-Days-of-Statistics/day3_Drawing-Marbles.py
@@ -4,7 +4,7 @@
 #A bag contains 3 red marbles and 4 blue marbles. Then, 2 marbles are drawn from the bag, at random, without replacement. If the first marble drawn is red, what is the probability that the second marble is blue?
 
 # Caculation:
-# P(A|B) = P(A&B)/P(B) = P(first draw is read and second draw is blue)/P(first draw is red) = (3/7)*(4/6)/(3/7) = 2/3
+# P(A|B) = P(A&B)/P(B) = P(two draw is read and blue)/P(first draw is red) = (3/7)*(4/6)/(3/7) = 2/3
 
 import random
 trials=100000
