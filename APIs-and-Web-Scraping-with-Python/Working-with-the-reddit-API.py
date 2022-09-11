f79f89df8fe90e8f3fb1245e7418c308301c5147
那須ゆえ <NasuYue@users.noreply.github.com>
2022-09-11 23:44:59 +0800

    .

--- APIs-and-Web-Scraping-with-Python/Working-with-the-reddit-API.py
+++ APIs-and-Web-Scraping-with-Python/Working-with-the-reddit-API.py
@@ -1,4 +1,4 @@
-# Task
+# ===== Task 1 ===== 
 # 1. Retrieving a list of trending posts on a particular subreddit
 # 2. Exploring the comments on a single article
 # 3. Posting your own comment on an article
@@ -17,6 +17,23 @@ response=requests.get(url,headers=headers,params=params)
 # Print the content of the response.  As you can see, this token corresponds to the account of Vik Paruchuri.
 python_top=response.json()
 
-
+# Note: 
 # Major request type: get, post(201), patch(200), delete(204)
 # Json: loads(), dumps()
+
+# ===== Task 2: find id with the most upvotes =====
+# Article item: data > children > {kind,data}
+python_top_articles=python_top['data']['children']
+
+print(type(python_top_articles[0]))
+print(len(python_top_articles))
+
+top_upvotes=0
+most_upvoted=0
+for item in python_top_articles:
+    if item['data']['ups']>top_upvotes:
+        top_upvotes=item['data']['ups']
+        most_upvoted=item['data']['id']
+    
+print(top_upvotes)
+print(most_upvoted)
