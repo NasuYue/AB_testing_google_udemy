5b6265f02e892a275f5c5a03be338e9bd64c3f2d
那須ゆえ <NasuYue@users.noreply.github.com>
2022-09-12 16:24:14 +0800

    .

--- APIs-and-Web-Scraping-with-Python/Working-with-the-reddit-API.py
+++ APIs-and-Web-Scraping-with-Python/Working-with-the-reddit-API.py
@@ -28,12 +28,43 @@ python_top_articles=python_top['data']['children']
 print(type(python_top_articles[0]))
 print(len(python_top_articles))
 
-top_upvotes=0
-most_upvoted=0
+most_upvoted = ""
+most_upvotes = 0
 for item in python_top_articles:
-    if item['data']['ups']>top_upvotes:
-        top_upvotes=item['data']['ups']
+    if item['data']['ups']>most_upvotes:
         most_upvoted=item['data']['id']
+        most_upvotes=item['data']['ups']
     
-print(top_upvotes)
 print(most_upvoted)
+print(most_upvotes)
+
+
+# ===== Task 4: Getting Post Comments =====
+# Follow reddit API "https://oauth.reddit.com/" and "/r/{subreddit}/comments/{article}" endpoint
+# Reference: https://old.reddit.com/dev/api#GET_comments_{article}
+
+new_adr='https://oauth.reddit.com/r/python/comments/4b7w9u'
+comments=requests.get(new_adr,headers=headers).json()
+
+# ==== Task 5: Getting the Most Upvoted Comment ====
+
+# Explore comments structure and find list of comment in 2nd item
+comments_list=comments[1]['data']['children']
+
+# Use Task 2 methold to find the most upvoted comment
+most_ups=0
+most_upvoted_comment=0
+for x in comments_list:
+    if x['data']['ups'] > most_ups:
+        most_ups=x['data']['ups']
+        most_upvoted_comment=x['data']['id']
+
+# ==== Task 6: Upvoting a Comment ====
+# Use id='d16y4ry' from task 5
+
+url='https://oauth.reddit.com/api/vote'
+payload={'id':'d16y4ry', 'dir':1}
+
+response=requests.post(url,json=payload,headers=headers)
+
+status=response.status_code
