b67168773105a7fe00602f4768e03f13ca4b0bc7
那須ゆえ <NasuYue@users.noreply.github.com>
2022-09-29 12:50:16 +0800

    Update script.py

--- Analyzing-Game-Sales/script.py
+++ Analyzing-Game-Sales/script.py
@@ -1,9 +1,13 @@
 import csv
+import os
 
 header=[]
 games=[]
 
-with open('game_sales.csv') as f:
+data_dir = os.path.dirname(__file__)
+file_path = os.path.join(data_dir, 'game_sales.csv')
+
+with open(file_path, mode='r', encoding="utf-8") as f:
     reader=csv.reader(f)
     rows=list(reader)
     header=rows[0]
@@ -14,4 +18,8 @@ for x in games:
     gs[x[0]]=float(x[-1])
     
 most_sold_game=sorted(gs.items(), key=lambda x:x[1],reverse=True)[0]
+# Tips: one line sorting a dict by desc
+
 most_sold_game=most_sold_game[0]
+
+print(most_sold_game)
