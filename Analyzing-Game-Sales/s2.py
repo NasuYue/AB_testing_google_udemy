91af7d4746d4dbcf719b084ab7379b6e20980f99
那須ゆえ <NasuYue@users.noreply.github.com>
2022-10-04 12:29:40 +0800

    .

--- Analyzing-Game-Sales/s2.py
+++
@@ -1,4 +0,0 @@
-# Task 1
-# Create a dictionary games_per_platform where each key is a platform with a value equal to the number of games published on that platform.
-# Assign to a variable named platform_with_most_games the platform with the most games.
-
