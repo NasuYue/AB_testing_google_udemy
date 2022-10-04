a5ec3ae596c8938cb785da79a287af918db24eb3
那須ゆえ <NasuYue@users.noreply.github.com>
2022-10-04 10:18:13 +0800

    .

---
+++ Analyzing-Game-Sales/s2.py
@@ -0,0 +1,4 @@
+# Task 1
+# Create a dictionary games_per_platform where each key is a platform with a value equal to the number of games published on that platform.
+# Assign to a variable named platform_with_most_games the platform with the most games.
+
--- Analyzing-Game-Sales/script.py
+++ Analyzing-Game-Sales/script.py
@@ -1,3 +1,6 @@
+# Task 1: 
+# Assign to a variable most_sold_game the name of the game with most global sales.
+
 import csv
 import os
 
@@ -5,21 +8,22 @@ header=[]
 games=[]
 
 data_dir = os.path.dirname(__file__)
-file_path = os.path.join(data_dir, 'game_sales.csv')
+file_path = os.path.join(data_dir, 'game_sales.csv') 
+# Handle directory path
 
 with open(file_path, mode='r', encoding="utf-8") as f:
     reader=csv.reader(f)
     rows=list(reader)
     header=rows[0]
     games=rows[1:]
+# Use with op to read lines
 
 gs={}
 for x in games:
     gs[x[0]]=float(x[-1])
     
 most_sold_game=sorted(gs.items(), key=lambda x:x[1],reverse=True)[0]
-# Tips: one line sorting a dict by desc
+# Use lambda to sort a dict by desc
 
 most_sold_game=most_sold_game[0]
-
 print(most_sold_game)
